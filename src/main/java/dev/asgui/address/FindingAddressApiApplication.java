983f8c68b4063508bf80e2e9366225ba5b59fda7
jefferson <jefferson_asg@hotmail.com>
2022-05-15 03:28:26 -0400

    feat:initial structure

---
+++ src/main/java/dev/asgui/address/FindingAddressApiApplication.java
@@ -0,0 +1,13 @@
+package dev.asgui.address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindingAddressApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindingAddressApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/asgui/address/FindingAddressApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.asgui.address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindingAddressApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
